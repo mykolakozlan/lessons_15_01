642ac9e5b817a7d31230e0f10ea420495ce61d31
nikolaskozlan <maxsuccess4@gmail.com>
2024-01-18 21:39:50 +0200

    added lesson 2

---
+++ lesson_2.py
@@ -0,0 +1,109 @@
+# Віртуальне середовище - навіщо його створювати та використовувати
+# Змінні в Python
+# Типи даних
+# Цілі числа
+# Арифметичні оператори
+# Введення даних із клавіатури. Функція input()
+
+NUMBER_PI = 3.14
+
+
+# box_number_1 = 1  #snake case
+#
+# months_in_year = 12
+#
+# a = 1
+# b = 2
+# c = a + b
+#
+# print(box_number_1)
+
+# незмінні
+
+# Numeric
+
+# int    1
+# complex   (1 + 1j)
+# float   1.1
+# decimal 1.2
+
+# str
+
+# bool
+# None
+
+# tuple
+
+# змінні
+
+# list
+# set
+# dict
+
+
+# value_1 = 0
+
+# value_1 = info    int
+# value_1 = link    list
+
+# value_1 += 1
+# # value_1 = value_1 + 1
+#
+#
+# value_2 = value_1
+#
+# print(value_1, type(value_1), id(value_1))    #1
+#
+# value_1 = "2"
+#
+# print(value_1, type(value_1),  id(value_1))     #?
+
+#
+# value_1 = 13
+# value_2 = 2
+#
+# result_add = value_1 + value_2
+# result_sub = value_1 - value_2
+# result_div_1 = value_1 / value_2
+# result_div_2 = value_1 // value_2 # цілочислнне ділення
+# result_div_3 = value_1 % value_2 # залишок від ділення
+# result_div_4 = divmod(value_1, value_2) # цілочислнне ділення тa залишок від ділення
+#
+# result_mul = value_1 * value_2
+# result_power = value_1 ** value_2
+# result_root = value_1 ** 0.5
+#
+# print(result_div_4, type(result_div_4))
+
+######## String ########
+
+# value_str = "hello"
+# value_str = 'hello I\'m\nNick'    #екранування   \n new line, \t add tab
+# value_str = """hello"""
+# value_str = '''hello'''
+
+# print(value_str)
+
+
+# name = "Nick"
+# age = 24
+#
+# result = "Hi I'm" + " " + name + ". " + "I am " + str(age) + "years old"
+# result = f"Hi I'm {name}. I am {age} years old"
+#
+#
+# print(result)
+
+
+######## input() ########    ALWAYS STRING
+
+# name = input("Please enter your name: ")
+# age = input("Please enter your age: ")
+# new_age = age + 1.1
+#
+# # result = "Hi I'm" + " " + name + ". " + "I am " + str(age) + "years old"
+# result = f"Hi I'm {name}. I am {age} years old. Next year I'll be {new_age} years old."
+#
+#
+# print(result)
+
